709290e8d6e58b2649e70c3c641b2b6faeb55899
k19038 <k19038@maslab.aitech.ac.jp>
2022-08-30 11:39:44 +0900

    [update] テーマの更新

--- chkraTheme.js
+++ chkraTheme.js
@@ -3,25 +3,56 @@ import { extendTheme } from "@chakra-ui/react";
 // example theme
 const theme = extendTheme({
   initialColorMode: 'light',
+  colors: {
+    primary: {
+      900: "#13502e",
+      800: "#1c6c47",
+      700: "#237c53",
+      600: "#298c61",
+      500: "#2e996d",
+      400: "#3da97f",
+      300: "#5BB993",
+      200: "#88cdb1",
+      100: "#b6e0cf",
+      50: "#e1f3ec",
+    },
+  },
   semanticTokens: {
     colors: {
       primary: {
-        default: "#5BB993",
-        _dark: "#65CCA3",
+        default: "primary.300",
+        _dark: "primary.300",
       },
       secondary: {
         default: "#D9D9D9",
         _dark: "#515151",
       },
+      secondaryHover: {
+        default: "#F2F9F6",
+        _dark: "#424242",
+      },
       bg: {
         default: "#FFFFFF",
         _dark: "#1C1C1C",
       },
+      // Agents
+      civilian: {
+        default: "#52D714",
+      },
+      ambulance: {
+        default: "#FFFFF0",
+      },
+      fire: {
+        default: "#D71414",
+      },
+      police: {
+        default: "#1462D7",
+      }
     },
   },
   fonts: {
     body: "system-ui, sans-serif",
-    heading: "Georgia, serif",
+    heading: "system-ui, sans-serif",
     mono: "Menlo, monospace",
   },
 });
