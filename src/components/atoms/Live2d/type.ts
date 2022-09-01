035f62a8bd00a699974218704a64fda4b2620e15
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-01 19:35:58 +0900

    [update] コンポーネントのPropsTypeを追加

--- src/components/atoms/Live2d/type.ts
+++ src/components/atoms/Live2d/type.ts
@@ -1,1 +1,19 @@
-export interface Props { }
+export interface Props {
+    width: number;
+    height: number;
+    top?: string;
+    left?: string;
+    right?: string;
+    bottom?: string;
+    color?: string;
+
+    ModelList?: string[];
+    TouchBody?: string[];
+    TouchHead?: string[];
+    TouchDefault?: string[];
+    PathFull?: string;
+    MobileShow?: boolean;
+
+    release?: boolean;
+    menuList?: string[];
+}
