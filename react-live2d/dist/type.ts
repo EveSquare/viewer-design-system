b7bc038fb7f127fd3b0eb77600438a42c8d38720
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-03 12:35:43 +0900

    [add] PropsのTypeを追加

---
+++ react-live2d/dist/type.ts
@@ -0,0 +1,12 @@
+interface Props {
+    ModelList: Array<string>;
+
+    width?: number;
+    height?: number;
+    top?: number;
+    right?: number;
+    left?: number;
+    bottom?: number;
+    MobileShow?: boolean;
+    release?: boolean;
+}
