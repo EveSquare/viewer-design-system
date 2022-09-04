707984518dbfaca2643aee07e4bf41afbcebb0b8
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-04 12:45:19 +0900

    [update] コメント削除

--- react-live2d/dist/index.js
+++ react-live2d/dist/index.js
@@ -21,6 +21,12 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function ReactLive2d(props) {
   // 好看颜色列表
   // green: '#B4DEAE',
@@ -28,53 +34,31 @@ function ReactLive2d(props) {
   // LightBlue: '#C8E6FE',
   // pink: '#F9B8BE'
   var width = props.width ? props.width : '300';
-  var height = props.height ? props.height : '500'; // 容器样式
+  var height = props.height ? props.height : '500';
+  var top = props.top ? props.top : '';
+  var right = props.right ? props.right : '0';
+  var bottom = props.bottom ? props.bottom : '0';
+  var left = props.left ? props.left : '';
+  var sizing = {
+    top: top,
+    right: right,
+    bottom: bottom,
+    left: left
+  }; // 容器样式
 
-  var containerStyle = {
+  var containerStyle = _objectSpread({
     position: 'fixed',
-    top: props.top ? props.top : '',
-    right: props.right ? props.right : '0',
-    bottom: props.bottom ? props.bottom : '0',
-    left: props.left ? props.left : '',
-    width: width
-  }; // canvas样式
-
-  var canvasStyle = {
-    position: 'relative',
-    top: props.top ? props.top : '',
-    right: props.right ? props.right : '0',
-    bottom: props.bottom ? props.bottom : '0',
-    left: props.left ? props.left : '',
+    width: width,
     height: height
-  }; // 面板主题样式
-  // let Theme = {
-  //     color: props.color ? props.color : '#C8E6FE',
-  //     width: '30px',
-  //     height: '30px',
-  // }
-  // let timer = null;
-  // const [controllerOn, setControllerOn] = useState(false)
-  // const [controllerIn, setControllerIn] = useState(false)
-  // const [printMenu, setPrintMenu] = useState(false)
-  // 进入显示控制台
-  // function cvMouseOver() {
-  //     setControllerOn(true)
-  // }
-  // function cvMouseOut() {
-  //     timer = setTimeout(() => {
-  //         // 0.01秒内没有进入点击面板，说明已经鼠标离开
-  //         if (!controllerIn) {
-  //             setControllerOn(false)
-  //             setControllerIn(false)
-  //         }
-  //     }, 10);
-  // }
+  }, sizing); // canvas样式
+
+
+  var canvasStyle = _objectSpread({
+    position: 'fixed'
+  }, sizing);
 
   (0, _react.useEffect)(function () {
-    props.ModelList ? LAppDefine.lappdefineSet.setModelDir(props.ModelList) : LAppDefine.lappdefineSet.setModelDir([]); // props.TouchBody ? LAppDefine.lappdefineSet.setHitBody(props.TouchBody) : LAppDefine.lappdefineSet.setHitBody([])
-    // props.TouchHead ? LAppDefine.lappdefineSet.setHitHead(props.TouchHead) : LAppDefine.lappdefineSet.setHitHead([])
-    // props.TouchDefault ? LAppDefine.lappdefineSet.setHitDefault(props.TouchDefault) : LAppDefine.lappdefineSet.setHitDefault([])
-    // props.PathFull ? LAppDefine.lappdefineSet.setPathFull(props.PathFull) : LAppDefine.lappdefineSet.setPathFull('')
+    props.ModelList ? LAppDefine.lappdefineSet.setModelDir(props.ModelList) : LAppDefine.lappdefineSet.setModelDir([]);
 
     if (!navigator.userAgent.match(/mobile/i) || props.MobileShow == true) {
       if (_lappdelegate.LAppDelegate.getInstance().initialize() == false) {
@@ -115,9 +99,7 @@ function ReactLive2d(props) {
     style: canvasStyle,
     width: width,
     height: height,
-    className: "live2d" // onMouseEnter={cvMouseOver}
-    // onMouseLeave={cvMouseOut}
-
+    className: "live2d"
   })));
 }
 
--- react-live2d/src/index.js
+++ react-live2d/src/index.js
@@ -14,62 +14,33 @@ function ReactLive2d(props) {
 
     const width = props.width ? props.width : '300';
     const height = props.height ? props.height : '500';
+    const top = props.top ? props.top : '';
+    const right = props.right ? props.right : '0';
+    const bottom = props.bottom ? props.bottom : '0';
+    const left = props.left ? props.left : '';
+
+    const sizing = {
+        top: top,
+        right: right,
+        bottom: bottom,
+        left: left,
+    }
 
     // 容器样式
     let containerStyle = {
         position: 'fixed',
-        top: props.top ? props.top : '',
-        right: props.right ? props.right : '0',
-        bottom: props.bottom ? props.bottom : '0',
-        left: props.left ? props.left : '',
         width: width,
+        height: height,
+        ...sizing
     }
     // canvas样式
     let canvasStyle = {
-        position: 'relative',
-        top: props.top ? props.top : '',
-        right: props.right ? props.right : '0',
-        bottom: props.bottom ? props.bottom : '0',
-        left: props.left ? props.left : '',
-        height: height,
+        position: 'fixed',
+        ...sizing
     }
-
-    // 面板主题样式
-    // let Theme = {
-    //     color: props.color ? props.color : '#C8E6FE',
-    //     width: '30px',
-    //     height: '30px',
-    // }
-
-    // let timer = null;
-
-    // const [controllerOn, setControllerOn] = useState(false)
-
-    // const [controllerIn, setControllerIn] = useState(false)
-
-    // const [printMenu, setPrintMenu] = useState(false)
-
-    // 进入显示控制台
-    // function cvMouseOver() {
-    //     setControllerOn(true)
-    // }
-
-    // function cvMouseOut() {
-    //     timer = setTimeout(() => {
-    //         // 0.01秒内没有进入点击面板，说明已经鼠标离开
-    //         if (!controllerIn) {
-    //             setControllerOn(false)
-    //             setControllerIn(false)
-    //         }
-    //     }, 10);
-    // }
 
     useEffect(() => {
         props.ModelList ? LAppDefine.lappdefineSet.setModelDir(props.ModelList) : LAppDefine.lappdefineSet.setModelDir([])
-        // props.TouchBody ? LAppDefine.lappdefineSet.setHitBody(props.TouchBody) : LAppDefine.lappdefineSet.setHitBody([])
-        // props.TouchHead ? LAppDefine.lappdefineSet.setHitHead(props.TouchHead) : LAppDefine.lappdefineSet.setHitHead([])
-        // props.TouchDefault ? LAppDefine.lappdefineSet.setHitDefault(props.TouchDefault) : LAppDefine.lappdefineSet.setHitDefault([])
-        // props.PathFull ? LAppDefine.lappdefineSet.setPathFull(props.PathFull) : LAppDefine.lappdefineSet.setPathFull('')
 
         if (!navigator.userAgent.match(/mobile/i) || props.MobileShow == true) {
 
@@ -96,9 +67,6 @@ function ReactLive2d(props) {
 
     return (
         <div>
-            {/* <button
-                onClick={LAppLive2DManager.onTap()}
-            >onTap</button> */}
             <div
                 style={containerStyle}
                 id="live2d-container"
@@ -121,8 +89,6 @@ function ReactLive2d(props) {
                     width={width}
                     height={height}
                     className="live2d"
-                // onMouseEnter={cvMouseOver}
-                // onMouseLeave={cvMouseOut}
                 ></canvas>
             </div>
         </div>
