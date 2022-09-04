7fac7080c2c264a248d0f112c97c0cf965a5f9dd
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-04 20:55:12 +0900

    [update] generalSettingStateFactory追加

---
+++ src/factories/generalSettingStateFactory.ts
@@ -0,0 +1,8 @@
+import { State } from "@/components/organisms/GeneralSettingModal/type";
+
+export const generalSettingState: State = {
+    colorMode: "dark",
+    headerVisibility: "show",
+    sideBarVisibility: "show",
+    characterVisibility: "show",
+}
