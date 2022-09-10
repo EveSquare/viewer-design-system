59e7684215099f0b4c9bd0c5ed8f324143b76069
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-10 23:19:28 +0900

    [update] logoのリンクを追加

--- src/components/organisms/Logo/Logo.tsx
+++ src/components/organisms/Logo/Logo.tsx
@@ -1,19 +1,24 @@
-import { Center, Text } from "@chakra-ui/react";
+import { Center, Link, Text } from "@chakra-ui/react";
 import React from "react";
 import { LogoImage } from "@/components/atoms/LogoImage";
+import { useTranslation } from 'react-i18next';
 
 export const Logo: React.FC = () => {
+  const { t, i18n } = useTranslation();
+
   return (
     <>
-      <Center>
-        <Text
-          as="b"
-          mr={2}
-          fontSize="2rem"
-          userSelect="none"
-        >RRSViewer</Text>
-        <LogoImage width={120} height={65} />
-      </Center>
+      <Link href="/" _hover={{ textDecoration: 'none' }} title={t("ホームへ戻る")}>
+        <Center>
+          <Text
+            as="b"
+            mr={2}
+            fontSize="2rem"
+            userSelect="none"
+          >RRSViewer</Text>
+          <LogoImage width={110} height={55} />
+        </Center>
+      </Link>
     </>
   );
 }
