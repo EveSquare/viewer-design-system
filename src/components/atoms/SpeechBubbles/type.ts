3c63b6c4f4701c6a92289fa51f906a5f80fd60ad
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-13 13:20:41 +0900

    [update] Messageの型定義追加

--- src/components/atoms/SpeechBubbles/type.ts
+++ src/components/atoms/SpeechBubbles/type.ts
@@ -1,2 +1,39 @@
-export interface Props {}
+import { PriorityForce, PriorityIdle, PriorityNone } from "../../atoms/SpeechBubbles/const";
 
+export interface Props { }
+
+interface Position {
+    x: number;
+    y: number;
+}
+
+export interface Message {
+    /**
+     * メッセージの内容
+     */
+    message: string;
+    /**
+     * 優先度の設定
+     */
+    priority: typeof PriorityNone | typeof PriorityIdle | typeof PriorityForce,
+    /**
+     * 作成日時
+     */
+    created_at: Date,
+    /**
+     * 更新日時
+     */
+    updated_at: Date,
+    /**
+     * 表示終了した日時
+     */
+    finished_at?: Date,
+    /**
+     * 表示可能日時
+     */
+    timeLimit_at: Date,
+    /**
+     * 発生位置
+     */
+    position: Position,
+}
