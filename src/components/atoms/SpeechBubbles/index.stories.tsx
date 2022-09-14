394ab08727805034f736da8b4113ffa554ec549a
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-14 21:21:25 +0900

    [update] Debug用のstoryを作成

--- src/components/atoms/SpeechBubbles/index.stories.tsx
+++ src/components/atoms/SpeechBubbles/index.stories.tsx
@@ -1,5 +1,6 @@
-import { Button } from "@chakra-ui/react"
+import { Box, Button, FormControl, Input } from "@chakra-ui/react"
 import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { useState } from "react"
 import { withDesign } from "storybook-addon-designs"
 import { SpeechBubbles } from "./"
 
@@ -16,21 +17,38 @@ export default {
 
 
 const Template: ComponentStory<typeof SpeechBubbles> = (args) => {
+
+  const [text, setText] = useState("　");
+  const [priority, setPriority] = useState(0);
+
   function onClickHandler() {
+    const dt = new Date();
+    dt.setHours(dt.getHours() + 1);
+    const timeLimit_at = dt;
+
+    const message = {
+      text: text,
+      priority: priority,
+      created_at: new Date(),
+      updated_at: new Date(),
+      timeLimit_at: timeLimit_at,
+      position: { x: 0, y: 0 },
+    }
+
     window.dispatchEvent(new CustomEvent('speechMessage', {
-      detail: {
-        message: String(new Date().getTime())
-      }
+      detail: message
     }))
   }
+
   return (
     <>
-      <Button
-        onClick={onClickHandler}
-        m={2}
-      >イベント発火
-      </Button>
-      <SpeechBubbles {...args} />
+      <SpeechBubbles {...args} isDebug={true} />
+      <Box m={5}></Box>
+      <FormControl>
+        <Input placeholder='テキスト' value={text} onChange={(event) => setText(event.target.value)} />
+        <Input placeholder='優先度' value={priority} onChange={(event => setPriority(Number(event.target.value)))} min={0} max={3} />
+        <Button onClick={onClickHandler}>送信</Button>
+      </FormControl>
     </>
   )
 }
