0b7f05784a6426afd8abbd892b051af194909ef5
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-14 22:31:56 +0900

    [update] MainViewerTemplateスタイル調整

--- src/components/templates/MainViewerTemplate/MainViewerTemplate.tsx
+++ src/components/templates/MainViewerTemplate/MainViewerTemplate.tsx
@@ -9,22 +9,30 @@ import { SliderKit } from "@/components/organisms/SliderKit";
 export const MainViewerTemplate: React.FC<Props> = ({ children, sideBarInfo, headerInfo, characterIsShowing, sliderArgs }) => {
     return (
         <>
-            <Box position="fixed">
-                <Flex w="100vw">
-                    <SideBar {...sideBarInfo} />
-                    <Header {...headerInfo} />
-                </Flex>
-            </Box>
-            <Box zIndex={2}>
-                <MessageArea isShowing={characterIsShowing} />
-            </Box>
-            <Box bg="bg" zIndex={1} position="absolute" bottom={0} width="100vw">
-                <Box px={10}>
-                    <SliderKit {...sliderArgs} />
+            <Box overflow={"hidden"} width={"100vw"} height={"100vh"} position={"relative"}>
+                <Box position="fixed">
+                    <Flex w="100vw">
+                        <SideBar {...sideBarInfo} />
+                        <Header {...headerInfo} />
+                    </Flex>
                 </Box>
-            </Box>
-            <Box zIndex={-1} w="100vw" h="100vh">
-                {children}
+                <Box
+                    zIndex={2}
+                    width={"80%"}
+                    position={"absolute"}
+                    bottom={"-25px"}
+                    right={"-40px"}
+                >
+                    <MessageArea isShowing={characterIsShowing} />
+                </Box>
+                <Box bg="bg" zIndex={1} position="absolute" bottom={0} width="100vw">
+                    <Box px={10}>
+                        <SliderKit {...sliderArgs} />
+                    </Box>
+                </Box>
+                <Box zIndex={-1} w="100vw" h="100vh">
+                    {children}
+                </Box>
             </Box>
         </>
     );
