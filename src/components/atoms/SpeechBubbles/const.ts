728480d3367f91ecf9d61345f5810866aece69f4
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-14 21:20:55 +0900

    [update] 表示するメッセージの定数定義

--- src/components/atoms/SpeechBubbles/const.ts
+++ src/components/atoms/SpeechBubbles/const.ts
@@ -1,3 +1,19 @@
+import { Message } from "./type";
+
 export const PriorityNone = 0;
+
 export const PriorityIdle = 1;
+
 export const PriorityForce = 2;
+
+// ミリ秒
+export const MessageUpdateCycle: number = 5000;
+
+export const BlankMessage: Message = {
+    text: null,
+    priority: PriorityNone,
+    created_at: new Date(),
+    updated_at: new Date(),
+    timeLimit_at: new Date(),
+    position: { x: 0, y: 0 },
+}
