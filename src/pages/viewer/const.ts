8fa83e9edd5aaeb75c0f8f29cfd9955b7aea5f55
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-17 22:36:51 +0900

    [update] Viewerの定数定義

---
+++ src/pages/viewer/const.ts
@@ -0,0 +1,21 @@
+export const LOG_BASE_PATH = "/Resources/logs/sample-logs/6";
+
+export const INITIAL_VIEW_STATE = {
+    target: [0, 0, 0],
+    rotationX: 30,
+    rotationOrbit: 0,
+    minZoom: 0,
+    maxZoom: 20,
+    zoom: 0.5
+};
+
+export const AGENT_COLOR = {
+    "TacticsPolice": [0, 0, 255],
+    "TacticsFire": [255, 0, 0],
+    "TacticsAmbulance": [255, 255, 255],
+    "Civilian": [0, 255, 0],
+}
+
+export const ICON_MAPPING = {
+    marker: { x: 0, y: 0, width: 128, height: 128, mask: true }
+};
