5b4374d8110a43f0cc41d742ef52fdb34d0d286b
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-17 22:33:20 +0900

    [update] 定数の宣言を追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname
+  },
+  publicRuntimeConfig: {
+    LOG_HOST: "http://localhost:3000",
+  }
 }
 
 module.exports = nextConfig
