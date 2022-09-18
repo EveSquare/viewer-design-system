e20911eda811292fa32620a97a9561d4bb5c0db0
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-18 15:40:53 +0900

    [update] LOG_HOST変更

--- next.config.js
+++ next.config.js
@@ -5,14 +5,14 @@ const nextConfig = {
     PROJECT_ROOT: __dirname
   },
   publicRuntimeConfig: {
-    LOG_HOST: "http://localhost:3000",
+    LOG_HOST: "https://raw.githubusercontent.com/",
   },
   experimental: {
     // ログのデータ(マップ・メタ)が多いため拡張 "デフォルトは128KBです。
     // しかし、2.7MBほどの大きさがあるため3MBとする。
     // 詳細: https://nextjs.org/docs/messages/large-page-data
     largePageDataBytes: 1024 * 1024 * 3, // 3MB
-  }
+  },
 }
 
 module.exports = nextConfig
