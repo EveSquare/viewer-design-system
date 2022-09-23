61fc42d537d4cca0a5e8374581f20733b6b15ebe
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-23 20:15:49 +0900

    [add] 市民の説明コンポーネントを追加

---
+++ src/factories/civilianExplanationComponent.tsx
@@ -0,0 +1,17 @@
+import { Text } from "@chakra-ui/react";
+import { ImageCard } from "@/components/molecules/ImageCard";
+
+export const CivilianExplanationComponent = () => {
+    return (
+        <>
+            <Text mb={4}>
+                市民は避難所に着くことを目指します。緑色で表示されます。
+            </Text>
+            <ImageCard
+                src="https://raw.githubusercontent.com/EveSquare/viewer-design-system/master/public/Resources/img/civilian.svg"
+                title="市民"
+                description="自力で避難所まで向かう"
+            />
+        </>
+    )
+}
