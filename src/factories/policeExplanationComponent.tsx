05d99d44050179a859f937125f928e78725b498c
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-23 20:16:30 +0900

    [add] 土木隊の説明コンポーネントを追加

---
+++ src/factories/policeExplanationComponent.tsx
@@ -0,0 +1,13 @@
+import { ImageCard } from "@/components/molecules/ImageCard";
+
+export const PoliceExplanationComponent = () => {
+    return (
+        <>
+            <ImageCard
+                src="https://raw.githubusercontent.com/EveSquare/viewer-design-system/master/public/Resources/img/police.svg"
+                title="土木隊"
+                description="道路を塞いでいるがれきを撤去"
+            />
+        </>
+    )
+}
