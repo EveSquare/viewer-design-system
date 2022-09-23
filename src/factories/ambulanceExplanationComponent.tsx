d88ff094d637672690e9b50c9ab2c480f30df960
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-23 20:15:26 +0900

    [add] 救急隊の説明コンポーネントの追加

---
+++ src/factories/ambulanceExplanationComponent.tsx
@@ -0,0 +1,13 @@
+import { ImageCard } from "@/components/molecules/ImageCard";
+
+export const AmbulanceExplanationComponent = () => {
+    return (
+        <>
+            <ImageCard
+                src="https://raw.githubusercontent.com/EveSquare/viewer-design-system/master/public/Resources/img/ambulance.svg"
+                title="救急隊"
+                description="自力で動けない市民を避難所まで搬送"
+            />
+        </>
+    )
+}
