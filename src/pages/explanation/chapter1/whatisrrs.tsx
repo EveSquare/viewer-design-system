e5b1230b8f59ddff78b5af3d191274a4ad758ab3
k19038 <k19038@maslab.aitech.ac.jp>
2022-09-29 13:23:51 +0900

    [update] RCRS -> RRSへ名称変更

--- src/pages/explanation/chapter1/whatisrrs.tsx
+++ src/pages/explanation/chapter1/whatisrrs.tsx
@@ -8,9 +8,9 @@ const WhatIsRRS: NextPage = () => {
     return (
         <>
             <ExplanationPage pageKey="whatisrrs">
-                <Heading mb={"2rem"}>RCRSとは</Heading>
+                <Heading mb={"2rem"}>RRSとは</Heading>
                 <Text>
-                    RoboCup Rescue Simulation(RCRS)とは都市直下型地震で被災した仮想都市における災害救助シミュレーションのことです。複数種類のロボットの協力による救助活動でいかに災害の被害を抑えられるかを競います。
+                    RoboCup Rescue Simulation(RRS)とは都市直下型地震で被災した仮想都市における災害救助シミュレーションのことです。複数種類のロボットの協力による救助活動でいかに災害の被害を抑えられるかを競います。
                 </Text>
                 <Box m={"4rem"}></Box>
                 <Text fontSize={"3xl"} mb={"2rem"}>目標は高いスコアを維持すること</Text>
