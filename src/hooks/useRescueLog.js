edea98e5ac7f1fdf467d43387a3439c4c40f6326
k19038 <k19038@maslab.aitech.ac.jp>
2022-10-12 16:24:03 +0900

    [update] useRescueLog型付け import整理

--- src/hooks/useRescueLog.js
+++
@@ -1,8 +0,0 @@
-import {useState, useEffect} from 'react';
-import data from '../data/sample-logs/6/full/1.json';
-
-function useRescueLog() {
-    const [rescuelog, setRescueLog] = useState(data);
-    return {rescuelog: rescuelog, setRescueLog: setRescueLog}
-}
-export default useRescueLog;
---
+++ src/hooks/useRescueLog.ts
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { LOG_BASE_PATH } from "@/common/viewer/const";
+import getConfig from "next/config";
+import { load } from '@loaders.gl/core';
+import { JSONLoader } from '@loaders.gl/json';
+
+async function useRescueLog() {
+
+    const host = getConfig().publicRuntimeConfig.LOG_HOST;
+    const rescueLogDataUrl = new URL(LOG_BASE_PATH + "/full/1.json", host).href;
+    const rescueLogData = await load(rescueLogDataUrl, JSONLoader);
+
+    const [rescuelog, setRescueLog] = useState(rescueLogData);
+    return { rescuelog: rescuelog, setRescueLog: setRescueLog }
+}
+export default useRescueLog;
