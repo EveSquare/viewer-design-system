8237dbc9ad5612373027c67deae866e017b8bcfe
k19038 <k19038@maslab.aitech.ac.jp>
2022-10-12 16:02:32 +0900

    [update] DafaultBlockadesLayer型付け import整理

--- src/RRSLayers/Blockades/DefaultBlockadesLayer.ts
+++ src/RRSLayers/Blockades/DefaultBlockadesLayer.ts
@@ -1,26 +1,24 @@
-import { OrthographicView, OrbitView, COORDINATE_SYSTEM } from "@deck.gl/core";
-import DeckGL, {
-  GeoJsonLayer,
-  ArcLayer,
-  PolygonLayer,
-  IconLayer,
-} from "deck.gl";
+import { COORDINATE_SYSTEM } from "@deck.gl/core";
+import { PolygonLayer } from "@deck.gl/layers";
 
 import normalizePosition from "../../lib/normalizePosition";
+import { MapInfo, Record } from "../../common/viewer/type";
 
 class BlockadesLayer {
-  constructor(mapdata) {
+  mapdata: MapInfo;
+
+  constructor(mapdata: MapInfo) {
     this.mapdata = mapdata;
   }
 
-  get(rescuelog) {
+  get(rescuelog: Record) {
     const np = new normalizePosition(this.mapdata.width, this.mapdata.height);
     const blockades = rescuelog.world.blockades
-      .filter((v) => {
+      .filter((v: any) => {
         return v.deleted !== true;
       })
-      .map((v) => {
-        let d = v.apexes.map((vv) => [np.getX(vv.x), np.getY(vv.y), 0]);
+      .map((v: any) => {
+        let d = v.apexes.map((vv: any) => [np.getX(vv.x), np.getY(vv.y), 0]);
         d.push([np.getX(v.apexes[0].x), np.getY(v.apexes[0].y), 0]); // push first vertex
         return {
           type: v.type,
@@ -41,8 +39,8 @@ class BlockadesLayer {
       filled: true,
       wireframe: true,
       lineWidthMinPixels: 1,
-      getPolygon: (d) => d.contour,
-      getElevation: (d) => d.elevation,
+      getPolygon: (d: any) => d.contour,
+      getElevation: (d: any) => d.elevation,
       getFillColor: [0, 0, 0, 254],
       getLineColor: [80, 80, 80],
       getLineWidth: 1,
