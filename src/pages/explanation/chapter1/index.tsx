9d379592f550b55f05dbb387a58850e1eb15ccaf
k19038 <k19038@maslab.aitech.ac.jp>
2022-10-12 18:18:52 +0900

    [add] チャプター１の総合ページ作成

---
+++ src/pages/explanation/chapter1/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from "next";
+
+import { WhatIsTheAgent } from "./whatistheagent";
+import { WhatIsRRS } from "./whatisrrs";
+import { Buildings } from "./buildings";
+import { ExplanationPage } from "@/components/pages/ExplanationPage";
+import { Box } from "@chakra-ui/react";
+
+const ChapterOne: NextPage = () => {
+
+    return (
+        <>
+            <ExplanationPage pageKey="">
+                <WhatIsRRS />
+                <Box m={"4rem"}></Box>
+                <WhatIsTheAgent />
+                <Box m={"4rem"}></Box>
+                <Buildings />
+            </ExplanationPage>
+        </>
+    )
+}
+
+
+export default ChapterOne;
