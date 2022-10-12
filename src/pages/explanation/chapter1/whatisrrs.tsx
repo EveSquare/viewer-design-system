5e787a332aebc45da0c042bbaec1cc72de877986
k19038 <k19038@maslab.aitech.ac.jp>
2022-10-12 18:18:37 +0900

    [update] それぞれのページのコンポーネントを分けた

--- src/pages/explanation/chapter1/whatisrrs.tsx
+++ src/pages/explanation/chapter1/whatisrrs.tsx
@@ -3,36 +3,44 @@ import { ExplanationPage } from "@/components/pages/ExplanationPage";
 import { Box, Heading, List, ListIcon, ListItem, Spacer, Text } from "@chakra-ui/react";
 import { MdCheckCircle } from 'react-icons/md';
 
+export const WhatIsRRS: React.FC = () => {
+    return (
+        <>
+            <Heading mb={"2rem"}>RRSとは</Heading>
+            <Text>
+                RoboCup Rescue Simulation(RRS)とは都市直下型地震で被災した仮想都市における災害救助シミュレーションのことです。複数種類のロボットの協力による救助活動でいかに災害の被害を抑えられるかを競います。
+            </Text>
+            <Box m={"4rem"}></Box>
+            <Text fontSize={"3xl"} mb={"2rem"}>目標は高いスコアを維持すること</Text>
+            <Text mb={4}>被害状況が悪化するとスコア(都市の価値)が減少します。</Text>
+            <List spacing={3}>
+                <ListItem>
+                    <ListIcon as={MdCheckCircle} color='green.500' />
+                    避難する市⺠を多く救助しよう
+                </ListItem>
+                <ListItem>
+                    <ListIcon as={MdCheckCircle} color='green.500' />
+                    瓦礫を除去し効率よく救助しよう
+                </ListItem>
+                <ListItem>
+                    <ListIcon as={MdCheckCircle} color='green.500' />
+                    ロボット同士で協力しよう
+                </ListItem>
+            </List>
+        </>
+    )
+}
 
-const WhatIsRRS: NextPage = () => {
+
+const WhatIsRRSPage: NextPage = () => {
     return (
         <>
             <ExplanationPage pageKey="whatisrrs">
-                <Heading mb={"2rem"}>RRSとは</Heading>
-                <Text>
-                    RoboCup Rescue Simulation(RRS)とは都市直下型地震で被災した仮想都市における災害救助シミュレーションのことです。複数種類のロボットの協力による救助活動でいかに災害の被害を抑えられるかを競います。
-                </Text>
-                <Box m={"4rem"}></Box>
-                <Text fontSize={"3xl"} mb={"2rem"}>目標は高いスコアを維持すること</Text>
-                <Text mb={4}>被害状況が悪化するとスコア(都市の価値)が減少します。</Text>
-                <List spacing={3}>
-                    <ListItem>
-                        <ListIcon as={MdCheckCircle} color='green.500' />
-                        避難する市⺠を多く救助しよう
-                    </ListItem>
-                    <ListItem>
-                        <ListIcon as={MdCheckCircle} color='green.500' />
-                        瓦礫を除去し効率よく救助しよう
-                    </ListItem>
-                    <ListItem>
-                        <ListIcon as={MdCheckCircle} color='green.500' />
-                        ロボット同士で協力しよう
-                    </ListItem>
-                </List>
+                <WhatIsRRS />
             </ExplanationPage>
         </>
     )
 }
 
 
-export default WhatIsRRS;
+export default WhatIsRRSPage;
--- src/pages/explanation/chapter1/whatistheagent.tsx
+++ src/pages/explanation/chapter1/whatistheagent.tsx
@@ -3,49 +3,57 @@ import { ExplanationPage } from "@/components/pages/ExplanationPage";
 import { Box, Flex, Heading, List, ListIcon, ListItem, Icon, Text, VStack, Grid, GridItem } from "@chakra-ui/react";
 import { ImageCard } from "@/components/molecules/ImageCard";
 
-const WhatIsTheAgent: NextPage = () => {
+export const WhatIsTheAgent: React.FC = () => {
+    return (
+        <>
+            <Heading mb={"2rem"}>人工知能ロボット(エージェント)の紹介</Heading>
+            <Text>
+                人工知能ロボットは互いに通信・協力しながら最適な災害救助活動を目指します。
+            </Text>
+            <VStack
+                spacing={4}
+                align='stretch'
+            >
+                <ImageCard
+                    src="/Resources/img/ambulance.svg"
+                    title="救急隊"
+                    description="自力で動けない市民を避難所まで搬送"
+                />
+                <ImageCard
+                    src="/Resources/img/fire.svg"
+                    title="消防隊"
+                    description="建物に埋まってしまって動けない市民と人工知能ロボットを救出"
+                />
+                <ImageCard
+                    src="/Resources/img/police.svg"
+                    title="土木隊"
+                    description="道路を塞いでいるがれきを撤去"
+                />
+            </VStack>
+            <Box m={"4rem"}></Box>
+            <Text fontSize={"3xl"} mb={"2rem"}>市民の紹介</Text>
+            <Text mb={4}>
+                市民は避難所に着くことを目指します。緑色で表示されます。
+            </Text>
+            <ImageCard
+                src="/Resources/img/civilian.svg"
+                title="市民"
+                description="自力で避難所まで向かう"
+            />
+        </>
+    )
+}
+
+const WhatIsTheAgentPage: NextPage = () => {
 
     return (
         <>
             <ExplanationPage pageKey="whatistheagent">
-                <Heading mb={"2rem"}>人工知能ロボット(エージェント)の紹介</Heading>
-                <Text>
-                    人工知能ロボットは互いに通信・協力しながら最適な災害救助活動を目指します。
-                </Text>
-                <VStack
-                    spacing={4}
-                    align='stretch'
-                >
-                    <ImageCard
-                        src="/Resources/img/ambulance.svg"
-                        title="救急隊"
-                        description="自力で動けない市民を避難所まで搬送"
-                    />
-                    <ImageCard
-                        src="/Resources/img/fire.svg"
-                        title="消防隊"
-                        description="建物に埋まってしまって動けない市民と人工知能ロボットを救出"
-                    />
-                    <ImageCard
-                        src="/Resources/img/police.svg"
-                        title="土木隊"
-                        description="道路を塞いでいるがれきを撤去"
-                    />
-                </VStack>
-                <Box m={"4rem"}></Box>
-                <Text fontSize={"3xl"} mb={"2rem"}>市民の紹介</Text>
-                <Text mb={4}>
-                    市民は避難所に着くことを目指します。緑色で表示されます。
-                </Text>
-                <ImageCard
-                    src="/Resources/img/civilian.svg"
-                    title="市民"
-                    description="自力で避難所まで向かう"
-                />
+                <WhatIsTheAgent />
             </ExplanationPage>
         </>
     )
 }
 
 
-export default WhatIsTheAgent;
+export default WhatIsTheAgentPage;
