7b356795fb7ad16203dcefbed6e3bdf46eacba9b
k19038 <k19038@maslab.aitech.ac.jp>
2022-10-12 16:10:13 +0900

    [update] Default ReadsLayers型付け import整理

--- src/RRSLayers/Roads/DefaultRoadsLayer.ts
+++ src/RRSLayers/Roads/DefaultRoadsLayer.ts
@@ -1,28 +1,22 @@
-import { OrthographicView, OrbitView, COORDINATE_SYSTEM } from "@deck.gl/core";
-import DeckGL, {
-  GeoJsonLayer,
-  ArcLayer,
-  PolygonLayer,
-  IconLayer,
-} from "deck.gl";
+import { AGENT_COLOR, ICON_MAPPING } from "@/common/viewer/const";
+import { MapInfo, AgentColor, IconMapping, Record } from "@/common/viewer/type";
+import { COORDINATE_SYSTEM } from "@deck.gl/core";
+import { PolygonLayer } from "@deck.gl/layers";
 
 import normalizePosition from "../../lib/normalizePosition";
 
 class RoadsLayer {
-  constructor(mapdata) {
+  mapdata: MapInfo;
+  AGENT_COLOR: AgentColor;
+  ICON_MAPPING: IconMapping;
+
+  constructor(mapdata: MapInfo) {
     this.mapdata = mapdata;
-    this.AGENT_COLOR = {
-      TacticsPolice: [0, 0, 255],
-      TacticsFire: [255, 0, 0],
-      TacticsAmbulance: [255, 255, 255],
-      Civilian: [0, 255, 0],
-    };
-    this.ICON_MAPPING = {
-      marker: { x: 0, y: 0, width: 128, height: 128, mask: true },
-    };
+    this.AGENT_COLOR = AGENT_COLOR;
+    this.ICON_MAPPING = ICON_MAPPING;
   }
 
-  get(rescuelog) {
+  get(rescuelog: Record) {
     const np = new normalizePosition(this.mapdata.width, this.mapdata.height);
 
     const roads = this.mapdata.entities
@@ -52,7 +46,7 @@ class RoadsLayer {
       filled: true,
       wireframe: true,
       lineWidthMinPixels: 1,
-      getPolygon: (d) => d.contour,
+      getPolygon: (d: any) => d.contour,
       getElevation: 0,
       getFillColor: [230, 230, 230],
       getLineColor: [80, 80, 80],
