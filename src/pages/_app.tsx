f7850e5b1260bc01fce1254dc60fd5dcf712cf1f
k19038 <k19038@maslab.aitech.ac.jp>
2022-10-20 13:07:44 +0900

    [update] global cssの読み込み

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
 import chkraTheme from "../../chkraTheme";
+import "@/styles/global.css";
 
 const theme = extendTheme(chkraTheme);
 
