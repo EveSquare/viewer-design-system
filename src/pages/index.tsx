75834c1f691d7adc8a273fd75e7c04a403a11867
k19038 <k19038@maslab.aitech.ac.jp>
2022-10-27 14:39:01 +0900

    [update] 多言語処理追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,60 @@
 import type { NextPage } from "next";
+import { LogList } from "@/components/templates/LogList";
+import { Props as LogCardProps } from "@/components/molecules/LogCard/type";
+import { LogSection as LogSectionProps } from "@/components/templates/LogList/type";
+import { useTranslation } from 'next-i18next';
 
 const Home: NextPage = () => {
+
+  const { t, i18n } = useTranslation();
+
+  const popularLogs: Array<LogCardProps> = [
+    {
+      title: t("Sample01"),
+      description: t("Sample01の説明文です。"),
+      href: "/viewer",
+      tags: [{
+        name: t("イベント"),
+        color: "bule",
+      }, {
+        name: t("サンプル"),
+        color: "red",
+      }],
+    },
+    {
+      title: t("Sample02"),
+      description: t("Sample02の説明文です。"),
+      href: "/viewer",
+      tags: [{
+        name: t("イベント"),
+        color: "bule",
+      }, {
+        name: t("サンプル"),
+        color: "red",
+      }],
+    },
+    {
+      title: t("Sample03"),
+      description: t("Sample03の説明文です。"),
+      href: "/viewer",
+      tags: [{
+        name: t("イベント"),
+        color: "bule",
+      }, {
+        name: t("サンプル"),
+        color: "red"
+      }],
+    },
+  ]
+
+  const logSections: Array<LogSectionProps> = [
+    { sectionName: t("人気"), logs: popularLogs }
+  ]
+
   return (
-    <h1>hi</h1>
+    <>
+      <LogList logSections={logSections} />
+    </>
   );
 };
 
