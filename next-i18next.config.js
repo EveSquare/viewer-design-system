83e34d84fae3044392e63f8be0e6ffb4efa14caa
k19038 <k19038@maslab.aitech.ac.jp>
2022-11-01 13:59:50 +0900

    [update] TODOを追記

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     i18n: {
         defaultLocale: 'ja',
-        locales: ['ja', 'en', 'ja_kana'],
+        locales: ['ja', 'en', 'ja_kana'], // TODO: ja_kana: カナ付きの日本語に対応する
     },
 };
