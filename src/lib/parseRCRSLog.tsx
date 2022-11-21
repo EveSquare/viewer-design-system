d2c41d20bc3c7e6bfe5c7705557cc7c1a464ad12
k19038 <k19038@maslab.aitech.ac.jp>
2022-11-21 12:45:08 +0900

    [update] パース部分をデシリアライズのみおこなう処理に変更

--- src/lib/parseRCRSLog.tsx
+++ src/lib/parseRCRSLog.tsx
@@ -1,53 +1,52 @@
-import protobuf from "protobufjs";
+//@ts-nocheck
 import { URN_MAP } from "./RCRSURN";
 import { Simulation } from "./RCRS";
+import "@/lib/proto/RCRSLogProto_pb.js";
 
-const read_int32 = (byte_array: Uint8Array) => {
-  return (
-    (byte_array[0] << 24) +
-    (byte_array[1] << 16) +
-    (byte_array[2] << 8) +
-    byte_array[3]
-  );
+const parse = (log: any) => {
+  switch (log.getLogCase()) {
+    case proto.LogProto.LogCase.START:
+      console.log("Start Record is Find");
+      break;
+    case proto.LogProto.LogCase.COMMAND:
+      // processCommand(log.getCommand());
+      console.log("Command Record is Find");
+      break;
+    case proto.LogProto.LogCase.CONFIG:
+      // processConfig(log.getConfig());
+      console.log("Config Record is Find");
+      break;
+    case proto.LogProto.LogCase.INITIALCONDITION:
+      // processInitCondition(log.getInitialcondition());
+      console.log("Initialcondition Record is Find");
+      break;
+    case proto.LogProto.LogCase.UPDATE:
+      // processUpdate(log.getUpdate());
+      console.log("Update Record is Find");
+      break;
+    case proto.LogProto.LogCase.PERCEPTION:
+      // processPerception(log.getPerception());
+      console.log("Perception Record is Find");
+      break;
+    case proto.LogProto.LogCase.END:
+      console.log("End Record is Find");
+      break;
+  }
 };
 
-export const parseRCRSLog = (log: any) => {
+export const parseRCRSLog = (binLog: any) => {
 
-  const sim = new Simulation();
+  // const sim = new Simulation();
 
-  protobuf.load(
-    [
-      "/Resources/logs/proto/RCRSLogProto.proto",
-      "/Resources/logs/proto/RCRSProto.proto",
-    ],
-    (err, root) => {
-      if (typeof root === "undefined") {
-        return;
-      }
-      const Message = root.lookupType("LogProto");
-
-      let r = new Uint8Array(log);
-
-      while (r.length > 0) {
-        const s = read_int32(r);
-        const c = r.slice(4, 4 + s);
-
-        const m = Message.decode(c);
-
-        console.log(m.toJSON());
-
-        // TODO: このままだとステップごとの差分データになっているので、fulllogにする
-        if (typeof m.toJSON().initialCondition !== "undefined") {
-          m.toJSON().initialCondition.entities.forEach((el: any) => {
-            const test = el.properties.map((v: any) => {
-              return { ...v, urn_name: URN_MAP[v.urn] };
-            });
-            console.log(el.entityID, URN_MAP[el.urn], test);
-          });
-        }
-
-        r = r.slice(4 + s);
-      }
-    }
-  );
+  const decoded_log = proto.LogProto.deserializeBinary(binLog);
+  parse(decoded_log);
+  // // TODO: このままだとステップごとの差分データになっているので、fulllogにする
+  // if (typeof m.toJSON().initialCondition !== "undefined") {
+  //   m.toJSON().initialCondition.entities.forEach((el: any) => {
+  //     const test = el.properties.map((v: any) => {
+  //       return { ...v, urn_name: URN_MAP[v.urn] };
+  //     });
+  //     console.log(el.entityID, URN_MAP[el.urn], test);
+  //   });
+  // }
 };
