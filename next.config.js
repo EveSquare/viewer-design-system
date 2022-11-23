df9a2f61d781e751bee8a67ebc7fcc271537713e
k19038 <k19038@maslab.aitech.ac.jp>
2022-11-23 23:46:09 +0900

    [update] 画像の設定を変えexport可能にした

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
   reactStrictMode: true,
@@ -14,8 +13,8 @@ const nextConfig = {
   },
   images: {
     domains: ["raw.githubusercontent.com"],
+    unoptimized: true
   },
-  i18n,
 }
 
 module.exports = nextConfig
