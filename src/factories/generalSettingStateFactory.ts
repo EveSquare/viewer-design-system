ac71847e5ba19d129b5ef3faac3bb678cf3288ea
k19038 <k19038@maslab.aitech.ac.jp>
2022-11-28 15:02:58 +0900

    [update] サイドバーをモバイルビューでは初期値としてhideを設定

--- src/factories/generalSettingStateFactory.ts
+++ src/factories/generalSettingStateFactory.ts
@@ -1,9 +1,10 @@
 import { State } from "@/components/organisms/GeneralSettingModal/type";
+import { isMobile } from "react-device-detect";
 
 export const generalSettingState: State = {
     colorMode: "dark",
     headerVisibility: "show",
-    sideBarVisibility: "show",
+    sideBarVisibility: isMobile ? "hide" : "show", //モバイル端末ではサイドバーを初期値で非表示とする
     characterVisibility: "show",
     sliderKitVisibility: "show",
 }
