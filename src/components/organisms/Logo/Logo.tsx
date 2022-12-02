a5fb59d2081567faa00e6010f4aa6e47db98c820
k19038 <k19038@maslab.aitech.ac.jp>
2022-12-02 14:43:52 +0900

    [update] ログ一覧のページ遷移用のリンクを追加

--- src/components/organisms/Logo/Logo.tsx
+++ src/components/organisms/Logo/Logo.tsx
@@ -6,9 +6,11 @@ import { useTranslation } from "next-export-i18n";
 export const Logo: React.FC = () => {
   const { t } = useTranslation();
 
+  const href = process.env.NEXT_PUBLIC_HOME_URL || "https://season-truffle-a3c.notion.site/2c888d84074247a6b86ef22d9dba3202?v=eff9d4455e25489aa9f515a8e1ee4847";
+
   return (
     <>
-      <Link href="/" _hover={{ textDecoration: 'none' }} title={t("ログ一覧へ")}>
+      <Link href={href} _hover={{ textDecoration: 'none' }} title={t("ログ一覧へ")}>
         <Tooltip label={t("ログ一覧へ")}>
           <Center>
             <LogoImage width={100} height={45} />
